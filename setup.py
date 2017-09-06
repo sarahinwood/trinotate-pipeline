651cec2bb04ace7c5f76d94bdc3fc13698599275
Tom Harrop <twharrop@gmail.com>
2017-09-06 23:27:20 +1200

    scripts as strings?

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name='trinotate_pipeline',
-    version='0.0.3',
+    version='0.0.4',
     description='python3 wrapper for Trinotate',
     long_description=readme(),
     url='https://github.com/sarahinwood/trinotate_pipeline',
