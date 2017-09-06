48f0f4c484d8a85829db42a80b498cd3a5bc763d
Tom Harrop <twharrop@gmail.com>
2017-09-06 22:01:11 +1200

    wrapped in __main__.py fixes #2

--- trinotate_pipeline/__main__.py
+++ trinotate_pipeline/__main__.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+import argparse
 import shutil
+import snakemake
 import subprocess
+import os
 import pathlib
+from pkg_resources import resource_filename
 
 
 #############
@@ -31,6 +35,8 @@ def check_binary_version(binary_path, version_suffix):
 # GLOBALS #
 ###########
 
+snakefile = resource_filename(__name__, 'Snakefile')
+
 binary_to_version_suffix = {
     'TransDecoder.LongOrfs': None,
     'TransDecoder.Predict': None,
@@ -45,22 +51,80 @@ binary_to_version_suffix = {
     'Trinotate': None
 }
 
+
 ########
 # MAIN #
 ########
 
-# get a dict of full paths to pass to snakemake
-binary_to_full_path = {}
-for binary in binary_to_version_suffix:
-    # check binary is in path
-    full_path = get_full_path(binary)
-    binary_to_full_path[binary] = full_path
-    # print full path
-    pref = 'Using {}'.format(binary)
-    print('{:>38}: {}'.format(pref, full_path))
-    # if we know how to check the version, do so and print
-    if binary_to_version_suffix[binary]:
-        suffix = binary_to_version_suffix[binary]
-        version = check_binary_version(full_path, suffix)
-        pref = '{} version'.format(binary)
-        print('{:>38}: {}'.format(pref, version))
+def main():
+    # parse arguments
+    parser = argparse.ArgumentParser(
+        prog='trinotate_pipeline')
+    parser.add_argument(
+        '--trinity_fasta',
+        required=True,
+        help='Trinity.fasta file to annotate',
+        type=str,
+        dest='trinity_fasta')
+    parser.add_argument(
+        '--blast_db',
+        required=True,
+        help='Uniprot database for BLAST searches, e.g. uniprot_sprot.pep',
+        type=str,
+        dest='blast_db')
+    parser.add_argument(
+        '--hmmer_db',
+        required=True,
+        help='Pfam database for use with hmmscan, e.g. Pfam-A.hmm',
+        type=str,
+        dest='hmmer_db')
+    parser.add_argument(
+        '--sqlite_db',
+        required=True,
+        help='Boilerplate Trinotate SQLite database, e.g. Trinotate.sqlite',
+        type=str,
+        dest='sqlite_db')
+    parser.add_argument(
+        '--outdir',
+        required=True,
+        help='Output directory',
+        type=str,
+        dest='outdir')
+    default_threads = min(os.cpu_count() // 2, 50)
+    parser.add_argument(
+        '--threads',
+        help=('Number of threads. Default: %i' % default_threads),
+        type=int,
+        dest='threads',
+        default=default_threads)
+    args = vars(parser.parse_args())
+
+    # get a dict of full paths to pass to snakemake
+    binary_to_full_path = {}
+    for binary in binary_to_version_suffix:
+        # check binary is in path
+        full_path = get_full_path(binary)
+        binary_to_full_path[binary] = full_path
+        # print full path
+        pref = 'Using {}'.format(binary)
+        print('{:>38}: {}'.format(pref, full_path))
+        # if we know how to check the version, do so and print
+        if binary_to_version_suffix[binary]:
+            suffix = binary_to_version_suffix[binary]
+            version = check_binary_version(full_path, suffix)
+            pref = '{} version'.format(binary)
+            print('{:>38}: {}'.format(pref, version))
+
+    # add the dict to args
+    args['binaries'] = binary_to_full_path
+
+    # run the pipeline
+    snakemake.snakemake(
+        snakefile=snakefile,
+        config=args,
+        cores=args['threads'],
+        timestamp=True)
+
+
+if __name__ == '__main__':
+    main()
