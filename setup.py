fb4381731f9f10d426d52e139051f5fe5f368f2b
Tom Harrop <twharrop@gmail.com>
2017-09-07 22:18:49 +1200

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name='trinotate_pipeline',
-    version='0.0.6',
+    version='0.0.7',
     description='python3 wrapper for Trinotate',
     long_description=readme(),
     url='https://github.com/sarahinwood/trinotate_pipeline',
