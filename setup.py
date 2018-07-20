4df2896f5606fae6f4e087e6761cffd513fd59b6
Tom Harrop <twharrop@gmail.com>
2018-07-20 14:16:05 +1200

    wd should be a param

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name='trinotate_pipeline',
-    version='0.0.8',
+    version='0.0.9',
     description='python3 wrapper for Trinotate',
     long_description=readme(),
     url='https://github.com/sarahinwood/trinotate_pipeline',
