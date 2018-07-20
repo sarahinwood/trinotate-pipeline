9bd8e19986f597924ed8b65d832506db0a7ae255
Tom Harrop <twharrop@gmail.com>
2018-07-20 14:19:00 +1200

    same for rnammer

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name='trinotate_pipeline',
-    version='0.0.9',
+    version='0.0.10',
     description='python3 wrapper for Trinotate',
     long_description=readme(),
     url='https://github.com/sarahinwood/trinotate_pipeline',
